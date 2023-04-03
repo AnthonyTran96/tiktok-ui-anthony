199df7f71e94cb4ec785cc4784a5e613a2aee475
AnthonyTran <quanghust96@gmail.com>
2023-04-04 00:02:50 +0700

    tao Button component

---
+++ src/components/Button/Button.js
@@ -0,0 +1,63 @@
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import classNames from 'classnames/bind';
+import styles from './Button.module.scss';
+
+const cx = classNames.bind(styles);
+
+function Button({
+    children,
+    to,
+    href,
+    leftIcon,
+    rightIcon,
+    primary = false,
+    light = false,
+    disable = false,
+    round = false,
+    large = false,
+    className,
+    ...passProps
+}) {
+    const props = { ...passProps };
+    const classes = cx('wrapper', {
+        primary,
+        light,
+        disable,
+        round,
+        large,
+        className,
+    });
+    let Button = 'button';
+    if (href) {
+        Button = 'a';
+        props.href = href;
+    }
+    if (to) {
+        Button = Link;
+        props.to = to;
+    }
+    return (
+        <Button className={classes} {...props}>
+            {leftIcon && <span className={cx('icon')}>{leftIcon}</span>}
+            <span className={cx('content')}>{children}</span>
+            {rightIcon && <span className={cx('icon')}>{rightIcon}</span>}
+        </Button>
+    );
+}
+
+Button.propType = {
+    children: PropTypes.node.isRequired,
+    to: PropTypes.string,
+    href: PropTypes.string,
+    leftIcon: PropTypes.node.isRequired,
+    rightIcon: PropTypes.node.isRequired,
+    primary: PropTypes.string,
+    light: PropTypes.bool,
+    disable: PropTypes.bool,
+    round: PropTypes.bool,
+    large: PropTypes.bool,
+    className: PropTypes.string,
+};
+
+export default Button;
--- src/layouts/components/Header/Header.js
+++ src/layouts/components/Header/Header.js
@@ -1,24 +1,55 @@
+import HeadlessTippy from '@tippyjs/react/headless';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEllipsisVertical, faRightToBracket } from '@fortawesome/free-solid-svg-icons';
 import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
+
 import styles from './Header.module.scss';
 import images from '~/assets/images';
 import Search from '~/layouts/components/Search';
+import Button from '~/components/Button';
+import Image from '~/components/Image';
+import config from '~/config';
 
 const cx = classNames.bind(styles);
 
 function Header() {
+    const renderTippy = (attrs) => (
+        <div className="box" tabIndex="-1" {...attrs}>
+            My tippy box
+        </div>
+    );
+
     return (
         <div className={cx('wrapper')}>
             <div className={cx('inner')}>
                 {/* logo */}
-                <div className={cx('logo')}>
-                    <img src={images.logo} alt="logo" />
-                </div>
+                <Link to={config.routes.root} className={cx('logo')}>
+                    <Image src={images.logo} alt="logo" />
+                </Link>
 
                 {/* search */}
                 <Search />
 
                 {/* actions */}
-                <div className="actions">actions</div>
+                <div className={cx('actions')}>
+                    <Button light to="/following">
+                        Upload
+                    </Button>
+                    <Button
+                        primary
+                        className={cx('login-btn')}
+                        href="https://app.axieinfinity.com/"
+                        leftIcon={<FontAwesomeIcon icon={faRightToBracket} />}
+                    >
+                        Log in
+                    </Button>
+                    <HeadlessTippy render={renderTippy}>
+                        <div className={cx('setting')}>
+                            <FontAwesomeIcon icon={faEllipsisVertical} />
+                        </div>
+                    </HeadlessTippy>
+                </div>
             </div>
         </div>
     );
